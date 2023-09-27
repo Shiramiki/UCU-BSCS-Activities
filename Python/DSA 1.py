c1436a34c72d7dd08f297e6e34f6f33fe77ce01a
Isooba Mbeiza Rachel <mbeizacokila@gmail.com>
2023-09-27 09:05:41 +0300

    Activity on introduction to Data Structures

---
+++ Python/DSA 1.py
@@ -0,0 +1,5 @@
+numbers = [5,3,8,2,7]
+
+total =  sum(numbers)
+
+print(f"The total is: {total}")
---
+++ Python/DSA 1_1.py
@@ -0,0 +1,5 @@
+numbers = [5,3,8,2,7]
+total = 0
+for num in numbers:
+    total += num
+print(f"The total is: {total}")
