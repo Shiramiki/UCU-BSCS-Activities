d3bc788d1a5b53c1521c6db5ef6f8ac1f7b57c95
Isooba Mbeiza Rachel <mbeizacokila@gmail.com>
2023-10-18 14:21:30 +0300

    Update smallest multiples.py

--- Python/10.18.2023/smallest multiples.py
+++ Python/10.18.2023/smallest multiples.py
@@ -4,8 +4,8 @@ def smallest_multiple(num):
     LCM = 1
     factors = []
     for i in range(num, 1, -1):
-        gcd = math.gcd(LCM, i)
-        LCM = (LCM * i) // gcd
+        GCD= math.gcd(LCM, i)
+        LCM = (LCM * i) // GCD
         factors.append(i)
     factors = factors[0:7]
     return LCM, factors
@@ -13,6 +13,5 @@ def smallest_multiple(num):
 n = 13
 smallest_multiple, factors = smallest_multiple(n)
 
-print("Factors:")
-print(factors)
+print(f"Factors: {factors}")
 print(f"Smallest Multiple of the first {n} numbers: {smallest_multiple}")
